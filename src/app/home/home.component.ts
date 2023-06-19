c7c33f792049684ee634225522a2d74b9e5ef4f1
Will Hoppin <whoppin@middlebury.edu>
2023-06-19 13:43:51 -0700

    new gif

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -80,8 +80,28 @@ export class HomeComponent implements OnInit {
   sponsors = [
     {
       image: '/assets/features/BurnOut.jpeg',
-      link: 'https://youtu.be/m3ILEWTkHVg'
-    }
+      link: 'https://www.focusfeatures.com/'
+    },
+    {
+      image: '/assets/features/BurnOut.jpeg',
+      link: 'https://www.linkedin.com/feed/update/urn:li:activity:7073111262141820928?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7073111262141820928%29'
+    },
+    {
+      image: '/assets/features/BurnOut.jpeg',
+      link: 'https://hoorae.co/'
+    },
+    {
+      image: '/assets/features/BurnOut.jpeg',
+      link: 'https://www.instagram.com/howard.thesis.showcase/'
+    },
+    {
+      image: '/assets/features/BurnOut.jpeg',
+      link: 'https://www.bcpdigital.com/'
+    },
+    {
+      image: '/assets/features/BurnOut.jpeg',
+      link: 'https://www.instagram.com/patrickorrphotography_/'
+    },
   ]
 
   constructor() { }
