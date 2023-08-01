d7c029827f1d79eba2fef5cf33b7f88ca7581c27
Will Hoppin <whoppin@middlebury.edu>
2023-08-01 11:40:16 -0400

    Remove related

--- src/app/feature-pages/coding/angel/angel.component.ts
+++ src/app/feature-pages/coding/angel/angel.component.ts
@@ -7,25 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AngelComponent implements OnInit {
 
-  related = [
-    {
-      image: '/assets/features/mb-code.png',
-      link: '/magic-binder'
-    },
-    {
-      image: '/assets/features/mb-com-code.png',
-      link: '/magic-binder-com'
-    },
-    {
-      image: '/assets/features/pi-code.png',
-      link: '/prod-ind'
-    },
-    {
-      image: '/assets/features/willflix-code.png',
-      link: '/willflix'
-    }
-  ]
-
   constructor() { }
 
   ngOnInit(): void {
