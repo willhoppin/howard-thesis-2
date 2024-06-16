e7e41480306de1c456a322e7056802abe7b34a35
williamhoppin <156711160+wwh8@users.noreply.github.com>
2024-06-16 02:12:36 +0100

    recap

--- src/app/info-pages/recap2024/recap2024.component.ts
+++ src/app/info-pages/recap2024/recap2024.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ImageData {
+  path: string;
+  aspectRatio: number;
+}
+
 @Component({
   selector: 'app-recap2024',
   templateUrl: './recap2024.component.html',
   styleUrls: ['./recap2024.component.scss']
 })
 export class Recap2024Component implements OnInit {
+  imagePaths: ImageData[] = [];
+  selectedImage: string | null = null;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadImages();
   }
 
+  loadImages(): void {
+    const folderPath = 'assets/recaps/2024/';
+    const imageCount = 257; // Total number of images
+
+    for (let i = 1; i <= imageCount; i++) {
+      const imagePath = `${folderPath}${i}.jpg`;
+      this.calculateAspectRatio(imagePath).then(aspectRatio => {
+        this.imagePaths.push({ path: imagePath, aspectRatio });
+        if (this.imagePaths.length === imageCount) {
+          this.imagePaths.sort((a, b) => a.aspectRatio - b.aspectRatio);
+          this.imagePaths.reverse(); // Reverse the order after sorting
+        }
+      });
+    }
+  }
+
+  calculateAspectRatio(imagePath: string): Promise<number> {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.onload = () => {
+        resolve(img.width / img.height);
+      };
+      img.src = imagePath;
+    });
+  }
+
+  openModal(imagePath: string): void {
+    this.selectedImage = imagePath;
+  }
+
+  closeModal(): void {
+    this.selectedImage = null;
+  }
 }
